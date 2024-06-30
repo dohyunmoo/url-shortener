078873251e2a61b4a27167218b399de34d7be830
Dohyun Moon <d6moon@uwaterloo.ca>
2024-06-30 15:09:42 -0400

    tailwindcss setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./static/*.html",
+    "./static/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
